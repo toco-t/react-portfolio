1b2a4073d5770517b0a6bb3e98bbc2ddb12b60e5
toco <ttachibana@my.bcit.ca>
2022-08-23 13:15:17 -0700

    New: update project section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,25 +6,25 @@ function Projects() {
 	const projects = [
 		{
 			id: 1,
-			title: "Discord Bot",
+			title: "Sibyl System - Discord bot",
 			stack: "Python | Discord API | Web sockets",
-			github: "https://github.com/toco-t",
+			github: "https://github.com/toco-t/sibyl-system",
 		},
 		{
 			id: 2,
-			title: "PokeDex",
-			stack: "React.js | MongoDB | Express.js | Node.js ",
+			title: "PokeDex - Web UI for PokéAPI with added features",
+			stack: "JavaScript | EJS | Node.js | Express | MongoDB | PokéAPI",
 			github: "https://github.com/toco-t/",
 		},
 		{
 			id: 3,
-			title: "NoteHub",
+			title: "NoteHub - Note sharing application",
 			stack: "JavaScript | EJS",
 			github: "https://github.com/toco-t/NoteHub",
 		},
 		{
 			id: 4,
-			title: "SwapOmen",
+			title: "SwapOmen - Bartering application",
 			stack: "HTML | CSS | JavaScript | Node.js | Express",
 			github: "https://github.com/LinnyPurple/2800-202210-DTC09",
 		},
